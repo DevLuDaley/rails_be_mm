787cdd5d39adcc310dd454428586dd5aefc5195e
devludaley <daleyluis@gmail.com>
2020-12-15 00:01:56 -0500

    use faker to mock up dummy seed data with meal names, images, urls, and cooking times

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,48 @@
+# frozen_string_literal: true
+
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+#Faker::Food.dish
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Meal.create([
-    { category: 'Breakfast', name: 'Eggs', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 15 },
-    
-    { category: 'Lunch', name: 'Caesar-Salad', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 10 },
-    { category: 'Lunch', name: 'Chicken-Salad', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 45 },
-    { category: 'Dinner', name: 'Baked-Ziti', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 75 },
-    { category: 'Dinner', name: 'Ackee & Saltfish', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 75 },
-    { category: 'Dinner', name: 'Baked-Alaskan', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 120 }
-               ])
+    { category: 'Breakfast', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "300x600", search_terms: ['cooking', 'meal', 'breakfast'], match_all: true), cooking_time: Faker::Number.between(from: 15, to: 45) },
+
+    { category: 'Breakfast', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "300x600", search_terms: ['cooking', 'meal', 'breakfast'], match_all: true), cooking_time: Faker::Number.between(from: 15, to: 45) },
+
+    { category: 'Breakfast', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "300x600", search_terms: ['cooking', 'meal', 'breakfast'], match_all: true), cooking_time: Faker::Number.between(from: 15, to: 45) },
+
+
+#! LUNCH
+
+    { category: 'Lunch', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'lunch'], match_all: true), cooking_time: 10 },
+
+    { category: 'Lunch', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'lunch'], match_all: true), cooking_time: 10 },
+
+    { category: 'Lunch', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'lunch'], match_all: true), cooking_time: 10 },
+
+
+
+    { category: 'Dinner', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'dinner'], match_all: true), cooking_time: 75 },
+
+
+    { category: 'Dinner', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'dinner'], match_all: true), cooking_time: 75 },
+
+
+    { category: 'Dinner', name: Faker::Food.dish, url: Faker::Internet.url(host: 'ThemDaleysBeCookin.com', path: '/yummy-image.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal', 'dinner'], match_all: true), cooking_time: 75 },
+
+
+    ])
 
 Recipe.create([
-    { category: 'Breakfast', name: 'Quiche', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 15 },
-    { category: 'Lunch', name: 'Hamburger', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 45 },
-    { category: 'Dinner', name: 'Pasta', url: 'www.rando-link.org', image_url: 'www.image-api.org', cooking_time: 25 }
+    { category: 'Breakfast', name: 'Quiche', url: Faker::Internet.url(host: 'example.com', path: '/foobar.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal'], match_all: true), cooking_time: 15 },
+
+    { category: 'Lunch', name: 'Hamburger', url: Faker::Internet.url(host: 'example.com', path: '/foobar.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal'], match_all: true), cooking_time: 45 },
+
+    { category: 'Dinner', name: 'Pasta', url: Faker::Internet.url(host: 'example.com', path: '/foobar.html'), image_url: Faker::LoremFlickr.image(size: "400x800", search_terms: ['cooking', 'meal'], match_all: true), cooking_time: 25 }
                ])
+
+            #    Faker::Internet.url(host: 'example.com', path: '/foobar.html')
